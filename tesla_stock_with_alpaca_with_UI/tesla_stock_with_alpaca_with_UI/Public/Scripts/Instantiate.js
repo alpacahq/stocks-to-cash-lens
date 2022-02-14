1a1961cdfa876e9ace0ac5acd8862e408c148713
coob113 <coob1113@gmail.com>
2022-02-14 11:23:54 +0100

    Updated UI

---
+++ tesla_stock_with_alpaca_with_UI/tesla_stock_with_alpaca_with_UI/Public/Scripts/Instantiate.js
@@ -0,0 +1,24 @@
+// -----JS CODE-----
+//@input Asset.ObjectPrefab hundred;
+//@input Asset.ObjectPrefab one;
+prevHighPrice = 0;
+// To avoid instantiating cash while users are dragging slider check if number was updated only
+// when finger was released. Number is a global representing current stock price.
+if(prevHighPrice != highPrice && updated){
+    const numberOfShares = 1;
+    var valueToInstantiate = number * numberOfShares;
+    var numberOfOnes = valueToInstantiate % 100;
+    var numberOfHundreds = (valueToInstantiate - numberOfOnes) / 100;
+    
+    for (var i=0; i<numberOfOnes; i++) {
+        script.one.instantiate(script.getSceneObject());
+    }
+
+    for (var j=0; j<numberOfHundreds; j++) {
+        script.hundred.instantiate(script.getSceneObject());
+    }
+
+    updated = false;
+    
+    prevHighPrice = highPrice;
+}
